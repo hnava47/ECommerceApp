7424dcb190bfcdb8ef7330e570a759f51771fca6
Hector Nava <hector.r.nava44@gmail.com>
2022-04-04 14:29:52 -0600

    Added routes to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import {
   LoginSide,
   SignUp
 } from './containers';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 export const App = () => {
   return (
-    <div>
-      <LoginSide/>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <div className="container">
+            <Routes>
+              <Route
+                path='/login'
+                element={<LoginSide/>}
+              />
+              <Route
+                path='/signup'
+                element={<SignUp/>}
+              />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
