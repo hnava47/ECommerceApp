c8790b8a2984942ace7d426c291382ce582f3dbb
ericbarriga <ericbarriga@MacBook-Pro.local>
2022-04-03 23:16:31 -0600

    added typeDefs and utils folder with auth.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,51 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    quantityOnHand: Int
+    unitPrice: Float
+    category: Category
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Cart {
+     _id: ID
+     products: [Product.name]
+
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+`;
+
 module.exports = typeDefs;
+//// ask why we need auth and checkout
+// ask about cart 
