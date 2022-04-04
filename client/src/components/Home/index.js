a2c72e85bca1f9d57dfb04170a07c9b2a9f15693
Hector Nava <hector.r.nava44@gmail.com>
2022-04-04 15:51:22 -0600

    Added home folder

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,6 @@
+export * from './FeaturedPost';
+export * from './Footer';
+export * from './Header';
+export * from './Main';
+export * from './MainFeaturedPost';
+export * from './Sidebar';
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,9 +11,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 // import Main from './Main';
 // import Sidebar from './Sidebar';
 // import Footer from './Footer';
-import post1 from '../components/Posts/blog-post.1.md';
-import post2 from '../components/Posts/blog-post.2.md';
-import post3 from '../components/Posts/blog-post.3.md';
+import post1 from '../components/Home/Posts/blog-post.1.md';
+import post2 from '../components/Home/Posts/blog-post.2.md';
+import post3 from '../components/Home/Posts/blog-post.3.md';
 
 import {
     Header,
