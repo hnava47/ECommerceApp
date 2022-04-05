9528974284c6388909a79546cdee1295167d4f4a
Hector Nava <hector.r.nava44@gmail.com>
2022-04-04 21:21:42 -0600

    Added modal for search

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,9 @@
-import * as React from 'react';
+import { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
@@ -7,12 +11,26 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 
+const style = {
+  position: 'absolute',
+  top: '25%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export const Header = (props) => {
   const { sections, title } = props;
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
-    <React.Fragment>
+    <Fragment>
       <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Typography
           component="h2"
@@ -24,9 +42,31 @@ export const Header = (props) => {
         >
           {title}
         </Typography>
-        <IconButton>
-          <SearchIcon />
+        <IconButton onClick={handleOpen}>
+          <SearchIcon/>
         </IconButton>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <Typography id="transition-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Fade>
+        </Modal>
         <IconButton>
           <ShoppingCartOutlinedIcon/>
         </IconButton>
@@ -49,7 +89,7 @@ export const Header = (props) => {
           </Link>
         ))}
       </Toolbar>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
