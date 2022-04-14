b157c848989c32870ef3d6932248ae7d10252459
Hector Nava <hector.r.nava44@gmail.com>
2022-04-14 17:36:58 -0600

    Added production path

--- server/server.js
+++ server/server.js
@@ -19,6 +19,10 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
     server.applyMiddleware({ app });
