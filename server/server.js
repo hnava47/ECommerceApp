a6a0286e8e75d53efcc3f0ea3c9bdd12020ab864
Hector Nava <hector.r.nava44@gmail.com>
2022-04-13 20:24:13 -0600

    Formatted server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
@@ -19,7 +18,6 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(authMiddleware());
 
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
