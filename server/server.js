274e3ca51ac4852686981fcf3ec4593fe6092d96
Hector Nava <hector.r.nava44@gmail.com>
2022-04-14 17:50:37 -0600

    Ran npm build

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+}
 
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
