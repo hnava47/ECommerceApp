59a637547f48030185418a1e3415344d047b4269
Hector Nava <hector.r.nava44@gmail.com>
2022-04-13 19:26:32 -0600

    Added mongoDB_uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost:27017/EcommerceApp', {
+connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/EcommerceApp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
