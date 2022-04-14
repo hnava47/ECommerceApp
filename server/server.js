c3ff4d4d95e74cbcba24c512d67a743d3f7f229f
Hector Nava <hector.r.nava44@gmail.com>
2022-04-14 17:54:32 -0600

    Added path

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
+const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
