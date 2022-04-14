2d7a249e67d073bb61dafb50f8dedd1e30ad931c
ericbarriga <ericbarriga@MacBook-Pro.local>
2022-04-13 23:43:27 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,16 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost:27017/EcommerceApp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// connect('mongodb://localhost:27017/EcommerceApp', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/EcommerceApp',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 module.exports = connection;
