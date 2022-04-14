a5aeba7f6fa2243b8eabefb463003e22fc538390
Hector Nava <hector.r.nava44@gmail.com>
2022-04-13 20:12:05 -0600

    Updated model field to categoryId

--- server/models/Product.js
+++ server/models/Product.js
@@ -19,10 +19,9 @@ const productSchema = new Schema({
         min: 0,
         default: 0
     },
-    category: {
+    categoryId: {
         type: Schema.Types.ObjectId,
         ref: 'Category',
-
     }
 });
 
